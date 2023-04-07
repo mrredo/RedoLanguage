ed815f7aa4f343937375ff183944995281ecfd51
mrredo <mrredogt@gmail.com>
2023-04-07 09:06:58 +0300

    added +=. -=, *=, /=, %=, = operators for variable assigning key = value key *= value

--- std/functions.go
+++ std/functions.go
@@ -7,6 +7,21 @@ var Functions = map[string]func(args ...interface{}) interface{}{
 		fmt.Print(args...)
 		return nil
 	},
+	"printf": func(args ...interface{}) interface{} {
+		if len(args) == 0 {
+			return ""
+		}
+		arg1, ok1 := args[0].(string)
+		if !ok1 {
+			fmt.Println("printf function requires first argument to be string")
+			return nil
+		}
+		if len(args) == 1 {
+			return arg1
+		}
+		fmt.Printf(arg1, args[1:]...)
+		return nil
+	},
 	"println": func(args ...interface{}) interface{} {
 		fmt.Println(args...)
 		return nil
